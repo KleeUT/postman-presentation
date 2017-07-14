2c8f08cc948586762a3faa806ac14855dc2d77c8
Klee Thomas <k.thomas@nib.com.au>
2017-07-14 10:16:13 +1000

    :art: cleaned up and added comments to help with demo

--- postman_tests/runner.js
+++ postman_tests/runner.js
@@ -13,15 +13,21 @@ describe("Postman: ", () => {
     envData = setEnvData(branch);
   }
   
+  // Bring in collections and data.
   let collections = findAllCollectionsAndData();
 
   for (let i = 0; i < collections.length; i++) {
     let collection = collections[i];
     const maxTimeout = 50000;
+
+    // Require data as object[]
     const data = collection.dataFile ? require(collection.dataFile) : [];
+    // require collection as json object
+    const postmanCollection = require(collection.collection);
+    
     const timeout = collection.dataFile ? data.length * maxTimeout : maxTimeout;
     const iterations = data.length;
-    const postmanCollection = require(collection.collection);
+
 
     // Use the name of the directory as the name of the Mocha test
     it(`Api Tests Collection - ${collection.name}`, done => {
@@ -50,12 +56,12 @@ function findAllCollectionsAndData() {
   let directories = fs
     .readdirSync(collectionsDir)
     .filter(file => fs.statSync(path.join(collectionsDir, file)).isDirectory());
+    // Build up collections and data files based on the directory convention
   return directories.map(dir => {
     let dataFilePath = path.join(collectionsDir, dir, "data.json");
     let result = {
       name: dir,
       collection: `./collections/${dir}/collection.json`,
-      // Collections and environments are JS Objects datafile is a path to the file
       dataFile: fs.existsSync(dataFilePath) ? dataFilePath : undefined
     };
     return result;
@@ -78,9 +84,9 @@ function runTest(name, collection, dataFile, environment, iterations, done) {
     .on("done", (err, summary) => {
       if (err || summary.run.failures.length > 0) {
         outputTestFailures(summary.run.failures);
-        // process.exit(1);
         done(new Error(`\n ${name} collection run encountered failures`));
       } else {
+        // obligatory party parrot
         console.log(`\n  ${name} collection run completed. :partyparrot:`);
         done();
       }
